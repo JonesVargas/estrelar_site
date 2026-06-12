44a597d3f68839426fb1ecdcea51d9ed56fbe53c
Jones Vargas <jonesvargas1980@gmail.com>
2026-06-12 07:48:21 -0300

    Ajusta arquivos estaticos no Railway

--- core/settings.py
+++ core/settings.py
@@ -138,7 +138,11 @@ USE_TZ = True
 # =========================
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATICFILES_DIRS = []
+if (BASE_DIR / "static").exists():
+    STATICFILES_DIRS = [BASE_DIR / "static"]
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # =========================
